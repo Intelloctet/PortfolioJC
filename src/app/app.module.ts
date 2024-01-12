d5262c9ffcbbdcee003c71008ee563449db2bce8
JEAN Christin -- io -- <jeanchristin.ig.network@gmail.com>
2024-01-12 22:10:04 +0300

    add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PortfolioModule } from "./portfolio/portfolio.module";
+import { HttpClient } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FooterModule } from './portfolio/footer/footer.module';
 
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
     declarations: [
         AppComponent
@@ -14,7 +23,19 @@ import { PortfolioModule } from "./portfolio/portfolio.module";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        PortfolioModule
+        PortfolioModule,
+        TranslateModule.forRoot(
+            {
+              defaultLanguage: 'en',
+              loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [HttpClient]
+              }
+            }
+          ),
+          FontAwesomeModule,
+          FooterModule
     ]
 })
 export class AppModule { }
